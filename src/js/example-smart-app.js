7c3ca665fc567bdb785885934a9051bbf5e24f9b
krishna-mindfire <krishna.k@mindfiresolutions.com>
2022-01-31 21:57:24 +0530

    changes

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -13,17 +13,18 @@
         var pt = myPatient.read();
         console.log(pt);
         console.log(smart.patient.request("Observation"));
-        var obv = myPatient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+        // var obv = myPatient.api.fetchAll({
+        //             type: 'Observation',
+        //             query: {
+        //               code: {
+        //                 $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+        //                       'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+        //                       'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+        //               }
+        //             }
+        //           });
 
+        var obv = smart.patient.request("Observation");
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
