b20081f2e86c593219e79a3cb65a888c1ef428b9
krishna-mindfire <krishna.k@mindfiresolutions.com>
2022-02-01 01:45:27 +0530

    changes

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -75,7 +75,7 @@
 
               p.hdl = getQuantityValueAndUnit(hdl[0]);
               p.ldl = getQuantityValueAndUnit(ldl[0]);
-              ret.resolve(p);
+              //ret.resolve(p);
             });
              
           })
