e851a58032d6eb6069d678b46dc8f348e90a3801
krishna-mindfire <krishna.k@mindfiresolutions.com>
2022-02-01 00:06:15 +0530

    changes

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -33,7 +33,7 @@
 
           var fname = '';
           var lname = '';
-console.log(patient);
+console.log(patient.name);
           if (typeof patient.name[0] !== 'undefined') {
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
